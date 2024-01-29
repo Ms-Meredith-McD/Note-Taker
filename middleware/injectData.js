4493bdd47d57438c19bcb4f40d5d819468bfca5d
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-01-29 16:22:21 -0600

    fixed injectData.js

--- middleware/injectData.js
+++ middleware/injectData.js
@@ -1,9 +1,7 @@
-const store = require("../db/store.json")
-const products = require("../db/products.json")
-const customers = require("../db/customers.json")
+const db = require("../db/db.json")
 
 function injectData(req, res, next) {
-    const database = { store, products, customers }
+    const database = { db }
     req.database = database
     next()
 }
