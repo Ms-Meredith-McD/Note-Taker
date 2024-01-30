5d740da2978f23c37135cb91f472ed1e648469bc
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-01-30 16:23:54 -0600

    found npm for unique note ids

---
+++ routes/api/notes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const injectData = require("../../middleware/injectData")
+const { v4: uuidv4 } = require('uuid');
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
--- routes/notes.routes.js
+++ routes/notes.routes.js
@@ -1,6 +1,7 @@
 // I'm going to put all my notes routes here
 const router = require("express").Router();
 const path = require('path');
+const fs = require('fs');
 
 // click link and load notes page
 router.get('/notes', (req, res) => {
@@ -8,8 +9,12 @@ router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-
-
+// get all the notes
+router.get('/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8')
+    const savedNotes = JSON.parse(note)
+    res.json(savedNotes);
+})
 
 
 
